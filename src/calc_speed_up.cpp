350ba51256cb286a9dbf98a031e5e039bdf0cbfc
Your Name <you@example.com>
2023-07-05 21:40:41 +0800

    update explanation of 'de_dis_uops_from_decoder'

--- src/calc_speed_up.cpp
+++ src/calc_speed_up.cpp
@@ -1,4 +1,6 @@
 #include "calc_speed_up.h"
+#include <filesystem>
+#include <fstream>
 
 static timestamp_t call_dgemm(dgemm_function dgemm);
 static timestamp_t call_dgemm(dgemm_function dgemm) {
@@ -23,18 +25,40 @@ static timestamp_t call_dgemm(dgemm_function dgemm) {
 
 void calc_speed_up(std::vector<Dgemm> &all_dgemm_inst) {
     std::vector<Dgemm> all_dgemm = all_dgemm_inst;
-
+    std::ofstream debug_file;
+    std::cout << "current dir:" << std::filesystem::current_path() << "\n";
+    std::string output_file_dir = "debug_block_log/";
+    std::string output_file = output_file_dir;
+    output_file.append("speed_etc.log");
+    if (!std::filesystem::is_directory(output_file_dir) ||
+        !std::filesystem::exists(
+            output_file_dir)) { // Check if src folder exists
+        std::filesystem::create_directory(output_file_dir); // create src folder
+    }
+    debug_file.open(output_file, std::ios_base::app | std::ios_base::out);
+    debug_file << "BLOCK_DENOMINATOR: " << BLOCK_DENOMINATOR << "\n";
     for (auto &v : all_dgemm) {
         v.m_epapsed_time = call_dgemm(v.m_f);
-
-        std::cout << std::setw(20) << v.m_name
-                  << ":  elapsed-time=" << std::setw(10) << v.m_epapsed_time;
-        if (v.m_name != "dgemm_basic") {
-            std::cout << "     speed-up=" << std::setw(10)
-                      << all_dgemm[0].m_epapsed_time /
-                             static_cast<double>(v.m_epapsed_time);
+        if (v.m_name == "dgemm_unrolled_avx256" ||
+            v.m_name == "dgemm_blocked_avx256") {
+            debug_file << std::setw(30) << v.m_name
+                       << ":  elapsed-time=" << std::setw(10)
+                       << v.m_epapsed_time;
+            debug_file << "     speed-up=" << std::setw(10)
+                       << all_dgemm[0].m_epapsed_time /
+                              static_cast<double>(v.m_epapsed_time);
+            debug_file << "\n";
+        } else {
+            std::cout << std::setw(30) << v.m_name
+                      << ":  elapsed-time=" << std::setw(10)
+                      << v.m_epapsed_time;
+            if (v.m_name != "dgemm_basic") {
+                std::cout << "     speed-up=" << std::setw(10)
+                          << all_dgemm[0].m_epapsed_time /
+                                 static_cast<double>(v.m_epapsed_time);
+            }
+            std::cout << "\n";
         }
-
-        std::cout << "\n";
     }
+    debug_file.close();
 }
--- src/dgemm_blocked_avx256.h
+++ src/dgemm_blocked_avx256.h
@@ -1,3 +1,6 @@
 #include <cstdint>
 void dgemm_blocked_avx256(const uint32_t n, const double *a, const double *b,
                           double *c);
+#ifndef BLOCK_DENOMINATOR
+#define BLOCK_DENOMINATOR 20
+#endif
